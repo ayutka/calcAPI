4fca41aeaf803d2518e1ec5e9fc1d2c3ddbeb730
ayutka <juventus1015@outlook.jp>
2021-11-30 18:10:45 +0900

    逆ポーランド記法の関数を独立化

--- calc.py
+++ calc.py
@@ -11,7 +11,7 @@ OPERATORS = {
 }
 
 #中置記法から後置記法へ変換
-def moji_to_postfix(moji): #関数内で入力値を後置記法に直す
+def Calc(moji): #関数内で入力値を後置記法に直す
     stack = [] #演算子
     postfix = [] #完成形
          
@@ -41,26 +41,23 @@ def moji_to_postfix(moji): #関数内で入力値を後置記法に直す
     while len(stack) != 0:
         postfix.append(stack.pop())
  
-    return postfix
+    return RPN_Calc(postfix)
 
 
 #逆ポーランド記法を計算
-def Calc(moji):
+def RPN_Calc(RPN:list):
     num_stack = [] #最終計算結果を格納
-    RPN = moji_to_postfix(moji)
+    #RPN = moji_to_postfix(moji)
     sp = 0 #スタックポインタを底に設定
     for i in RPN: #数値と演算子を判別
         if '!' <= i <= "'" or ':' <= i <= ']' or '_' <= i <= '~' or i == ',' or i == '.':
             print('その入力では計算できません。')
             break
         elif i in ['+', '-', '*', '/', '^']: #文字判別　iに[]の中身が含まれているか
-            num2 = num_stack.pop() #最後尾から取り出し
-            sp -= 1
-            if sp <= 0: #spがstackの底以下を指している時
-                print('計算エラー発生')
-                continue
-            else:
-                num1 = num_stack.pop() #num2のエラー
+            try:
+                num2 = num_stack.pop() #最後尾から取り出し
+                sp -= 1                
+                num1 = num_stack.pop()
                 sp -= 1
                 if i == '+':
                     sp += 1
@@ -72,10 +69,10 @@ def Calc(moji):
                     sp += 1
                     num_stack.append(num1*num2)
                 elif i == '/':
-                    if num2 == 0: #0は除算できない
+                     if num2 == 0: #0は除算できない
                         print('error')
                         exit() #exitは関数 
-                    else:
+                     else:
                         sp += 1
                         num_stack.append(num1/num2)
                 elif i == '^':
@@ -84,8 +81,8 @@ def Calc(moji):
                         num_stack.append(1)
                     else:
                         num_stack.append(num1**num2)  
-                        print('a',num1)
-                        print('b',num2)
+            except IndexError as e:
+                return 'error'
         elif i in ['(', ')']: #カッコが来たら
             continue
         else:
@@ -93,7 +90,6 @@ def Calc(moji):
             num_stack.append(int(i)) #数値スタックに数値のみを格納
             
     return num_stack[0]
-
 
 if __name__ == "__main__": #実質メイン関数
     moji = input().split(' ') #' 'で文字列を分割
